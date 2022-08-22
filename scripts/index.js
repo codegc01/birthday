969cec499758a5054c745f163af0b360a53a8738
coding wala bhaiya <108386089+codegc01@users.noreply.github.com>
2022-08-22 06:37:03 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'jan 28, 2022',
+  birthdate: 'aug 22, 2022',
   name: 'Darlene'
 };
 
